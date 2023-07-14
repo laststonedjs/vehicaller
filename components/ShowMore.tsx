77816499212f29749f8af09689c991915bd3eaf0
Filip <filipsarovic428@gmail.com>
2023-07-14 18:45:49 +0200

    Finished with Show More functionality

---
+++ components/ShowMore.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+// types
+import { ShowMoreProps } from "@/types";
+
+import { useRouter } from "next/navigation";
+// components
+import CustomButton from "./CustomButton";
+// utils
+import { updateSearchParams } from "@/utils";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathName);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
